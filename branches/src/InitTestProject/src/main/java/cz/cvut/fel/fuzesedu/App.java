e225254862df9fc32f1877f8be38ea93217059a2
fuzesed <fuzesed@fe308385-ffec-4f70-837b-37f6d37e80a1>
2014-10-12 13:21:41 +0000

    InitTestProject opraveny a fungujuci

--- branches/src/InitTestProject/src/main/java/cz/cvut/fel/fuzesedu/App.java
+++ branches/src/InitTestProject/src/main/java/cz/cvut/fel/fuzesedu/App.java
@@ -7,18 +7,24 @@ import cz.cvut.fel.fuzesedu.user.DBUser;
  
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Maven + Hibernate + Oracle");
+		System.out.println("Maven + Hibernate Annotation + Oracle");
 		Session session = HibernateUtil.getSessionFactory().openSession();
- 
+
 		session.beginTransaction();
 		DBUser user = new DBUser();
- 
+
 		user.setUserId(100);
-		user.setUsername("superman");
+		user.setUsername("Hibernate101");
 		user.setCreatedBy("system");
 		user.setCreatedDate(new Date());
- 
+
 		session.save(user);
 		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		DBUser user2 =  (DBUser) session.get(DBUser.class, 100);
+		System.out.println(">> " + user2.getUsername());
+		session.getTransaction().commit();
+		
 	}
 }
