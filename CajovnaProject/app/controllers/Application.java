a5943144c24185ed9840212602ac911bce4b7299
fuzesed <fuzesed@fe308385-ffec-4f70-837b-37f6d37e80a1>
2014-11-01 21:37:13 +0000

    play init done

--- CajovnaProject/app/controllers/Application.java
+++ CajovnaProject/app/controllers/Application.java
@@ -1,15 +1,27 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import play.*;
 import play.mvc.*;
-
+import play.db.*; 
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
     	
-    	return ok(index.render("Your new application is not ready."));
+    	Connection connection = DB.getConnection();
+    	
+    	try {
+			return ok(index.render("Your new application is ready. " + connection.getCatalog()));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return ok(index.render("Your new application is not ready.")); 
+		}
     }
 
 }
