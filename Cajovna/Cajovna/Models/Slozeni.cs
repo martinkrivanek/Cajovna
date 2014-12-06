acdbcf756e7b0c7ccbbd9d583aa6011fd6d981ff
nedvevoj <nedvevoj@fe308385-ffec-4f70-837b-37f6d37e80a1>
2014-12-06 22:25:59 +0000

    create all entities

---
+++ Cajovna/Cajovna/Models/Slozeni.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cajovna.Models
+{
+    public class Slozeni
+    {
+        public int slozeniID { get; set; }
+
+        [Required]
+        public int quantity { get; set; }
+
+        [Required]
+        public int surovinaID { get; set; }
+        public virtual Surovina surovina { get; set; }
+
+
+        // METHODs
+        internal double price()
+        {
+            return quantity * surovina.price;
+        }
+    }
+}
---
+++ Cajovna/Cajovna/Models/Stul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cajovna.Models
+{
+    public class Stul
+    {
+        public int stulID { get; set; }
+
+        public String name { get; set; }
+
+        public virtual List<Ucet> ucty { get; set; }
+
+
+        // CONTSTRUCTORs
+        public Stul()
+        {
+            name = "Stůl #" + stulID;
+            ucty = new List<Ucet>();
+        }
+    }
+}
